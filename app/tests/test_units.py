78b498e3ba60645a8a63f3bd9abbc4593d0ac8ef
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-11-02 14:33:34 +0100

    :memo: Add tests documentation

--- app/tests/test_units.py
+++ app/tests/test_units.py
@@ -21,7 +21,8 @@ def test_create_unit():
 
 
 def test_fetch_all_units():
-    response = client.get("/units")
+    """Makes a get request in the route /units/ to fetch all units"""
+    response = client.get("/units/")
     assert response.status_code == 200
 
     data = response.json()
@@ -29,13 +30,21 @@ def test_fetch_all_units():
 
 
 def test_fetch_one_unit():
+    """Makes a get request in the route /units/{unit} for the unit 'foo' to fetch the unit"""
     response = client.get("/units/foo")
     assert response.status_code == 200
     data = response.json()
     assert data["data"] == "foo"
 
 
+def test_fetch_nonexistent_unit():
+    """Makes a get request in the route /units/{unit} for the unit 'zzz' to try to fetch the nonexistent unit"""
+    response = client.get("/units/zzz")
+    assert response.status_code == 404
+
+
 def test_put_unit():
+    """Makes a put request in the route /units/{unit} for the unit 'foo' to become 'foobar'"""
     response = client.put(
         "/units/foo",
         json={"unit": "foobar"},
@@ -53,17 +62,22 @@ def test_put_unit():
 
 
 def test_patch_unit():
+    """Makes a patch request in the route /units/{unit} for the unit 'foobar' to become 'foo'"""
     response = client.patch(
         "/units/foobar",
         json={"unit": "foo"},
     )
     assert response.status_code == 204
 
+    response = client.get("/units/foobar")
+    assert response.status_code == 404
+
     response = client.get("/units/foo")
     assert response.status_code == 200
 
 
 def test_empty_patch_unit():
+    """Makes a patch request in the route /units/{unit} for the unit 'foo' with an empty payload"""
     response = client.patch("/units/foo", json={})
     assert response.status_code == 204
 
@@ -75,6 +89,7 @@ def test_empty_patch_unit():
 
 
 def test_delete_unit():
+    """Makes a delete request in the route /units/{unit} for the unit 'foo'"""
     response = client.delete("/units/foo")
     assert response.status_code == 204
 
