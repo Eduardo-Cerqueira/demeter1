cb4e3ece654fe29f10be5ed80ebf11d31638f4d3
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-11-02 16:39:47 +0100

    :card_file_box: Add CRUD for production table

--- app/persistence/production_repository.py
+++ app/persistence/production_repository.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import psycopg2
 import os
 from dotenv import load_dotenv
@@ -21,11 +23,11 @@ conn = psycopg2.connect(
 db = conn.cursor()
 
 
-def fetch_all_production() -> list[str] | Exception:
+def fetch_all_production() -> list[tuple[Any, ...]] | Exception:
     """Returns a list of string representing all productions inside the table production.
     If this nothing is found, it will return a single empty array.
-    :return: A list of string unit
-    :rtype: list[str] | Exception
+    :return: A list of tuples containing string unit
+    :rtype: list[tuple[str]] | Exception
     """
     try:
         db.execute("SELECT * FROM production")
@@ -34,11 +36,11 @@ def fetch_all_production() -> list[str] | Exception:
         return error
 
 
-def fetch_production_by_code(code: str) -> list[str] | Exception:
+def fetch_production_by_code(code: int) -> tuple[Any, ...] | None | Exception:
     """Returns a list of string representing a production filtered by the code field inside the table production.
     If this nothing is found, it will return a single empty array.
-    :return: A list of string unit
-    :rtype: list[str] | Exception
+    :return: A list of tuples containing string unit
+    :rtype: list[tuple[str]] | Exception
     """
     try:
         db.execute("SELECT * FROM production WHERE code = %s", [code])
@@ -47,25 +49,9 @@ def fetch_production_by_code(code: str) -> list[str] | Exception:
         return error
 
 
-def insert_production(unit: str, name: str) -> None | Exception:
-    """Insert production row into table production.
-    :parameter unit:
-    :parameter name:
-    :return: Nothing or an error
-    :rtype: None | Exception
-    """
-    try:
-        db.execute("INSERT INTO production(unit, name) VALUES(%s,%s)", [unit, name])
-        print(type(db.lastrowid))
-    except Exception as error:
-        return error
-    finally:
-        conn.commit()
-
-
-def update_production(unit: str, name: str) -> None | Exception:
-    """
-    Update production row using his code field to filter from table production.
+def insert_production(code: int, unit: str, name: str) -> None | Exception:
+    """Insert production into table production.
+    :parameter code:
     :parameter unit:
     :parameter name:
     :return: Nothing or an error
@@ -73,7 +59,8 @@ def update_production(unit: str, name: str) -> None | Exception:
     """
     try:
         db.execute(
-            "UPDATE production SET unit = %s, name = %s WHERE code = %s", [unit, name]
+            "INSERT INTO production(code, unit, name) VALUES(%s,%s,%s)",
+            [code, unit, name],
         )
     except Exception as error:
         return error
@@ -81,7 +68,53 @@ def update_production(unit: str, name: str) -> None | Exception:
         conn.commit()
 
 
-def delete_production(code: str) -> None | Exception:
+def update_production(
+    code: int, new_code: int, new_unit: str, new_name: str
+) -> None | Exception:
+    """
+    Update production row using his code field to filter from table production.
+    :parameter code:
+    :parameter new_code:
+    :parameter new_unit:
+    :parameter new_name:
+    :return: Nothing or an error
+    :rtype: None | Exception
+    """
+    try:
+        db.execute(
+            "UPDATE production SET code = %s, unit = %s, name = % WHERE code = %s",
+            [new_code, new_unit, new_name, code],
+        )
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+
+
+def partial_update_production(
+    code: int, new_code: int, new_unit: str, new_name: str
+) -> None | Exception:
+    """Partially update production row using his code field to filter from table unit.
+    :parameter code:
+    :parameter new_code:
+    :parameter new_unit:
+    :parameter new_name:
+    :return: Nothing or an error
+    :rtype: None | Exception
+    """
+    try:
+        db.execute(
+            "UPDATE production SET code = COALESCE(%s, code), unit = COALESCE(%s, unit), name = COALESCE(%s, "
+            "name) WHERE code = %s",
+            [new_code, new_unit, new_name, code],
+        )
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+
+
+def delete_production(code: int) -> None | Exception:
     """
     Delete production row using his code field to filter from table production.
     :parameter code:
