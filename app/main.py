377fe64c21563e8b8d862b6b010965ef5e8c591c
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-11-02 19:15:51 +0100

    :sparkles: Add productions routes to main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from app.routers import units
+from fastapi import FastAPI
+from app.routers import units, productions
 
 app = FastAPI(title="Demeter", version="0.1.0")
 
 
 app.include_router(units.router)
+app.include_router(productions.router)
 
 
 @app.get("/")
