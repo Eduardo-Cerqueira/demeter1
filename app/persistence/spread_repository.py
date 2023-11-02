cb02619e30276df923dd572f84b9153c686fb6a6
Val <pro.mvaldo@gmail.com>
2023-11-02 23:54:21 +0100

    :art: change spread repo code structure

--- app/persistence/spread_repository.py
+++ app/persistence/spread_repository.py
@@ -31,23 +31,28 @@ def get_spreads():
     """
     Get all the spread in the database.
 
-    :return: A list of all spread.
+    :return: A list of all spread or an Exception.
     """
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM spread")
-    spreads_data = cur.fetchall()
-    spreads_list = []
-    for spread in spreads_data:
-        spread_dict = {
-            "fertilizer_id": spread[0],
-            "plot_number": spread[1],
-            "date": spread[2],
-            "spread_quantity": float(spread[3]),
-        }
-        spreads_list.append(spread_dict)
-    conn.close()
-    return spreads_list
+    try:
+        cur.execute("SELECT * FROM spread")
+        spreads_data = cur.fetchall()
+        spreads_list = []
+        for spread in spreads_data:
+            spread_dict = {
+                "fertilizer_id": spread[0],
+                "plot_number": spread[1],
+                "date": spread[2],
+                "spread_quantity": float(spread[3]),
+            }
+            spreads_list.append(spread_dict)
+            return spreads_list
+    except Exception as error:
+        return error
+    finally:
+        conn.close()
+
 
 
 def get_spread_by_fertilizer(fertilizer_id):
@@ -55,28 +60,27 @@ def get_spread_by_fertilizer(fertilizer_id):
     Get a spread by fertilizer_id.
 
     :param (str) fertilizer_id: The ID of the fertilizer.
-    :return: A list of dict with the spread's data or None if not found.
+    :return: A list of dict with the spread's data, None or Error if not found.
     """
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM spread WHERE fertilizer_id = %s",
-        (fertilizer_id,),
-    )
-    spread = cur.fetchone()
-    conn.close()
+    try:
 
-    if spread is None:
-        return None
-
-    spread_dict = {
-        "fertilizer_id": spread[0],
-        "plot_number": spread[1],
-        "date": spread[2],
-        "spread_quantity": float(spread[3]),
-    }
-
-    return spread_dict
+        cur.execute(
+            "SELECT * FROM spread WHERE fertilizer_id = %s",
+            (fertilizer_id,),
+        )
+        spread_dict = {
+            "fertilizer_id": spread[0],
+            "plot_number": spread[1],
+            "date": spread[2],
+            "spread_quantity": float(spread[3]),
+        }
+        return spread_dict
+    except Exception as error:
+        return error
+    finally:
+        conn.close()
 
 
 def create_spread(fertilizer_id, plot_number, date, spread_quantity):
@@ -87,19 +91,20 @@ def create_spread(fertilizer_id, plot_number, date, spread_quantity):
     :param (int) plot_number: The plot number.
     :param (date) date: The date of the spread.
     :param (int) spread_quantity: The quantity of the spread.
+    :return: Exception if already exist.
     """
-    existing_spread = get_spread_by_fertilizer(fertilizer_id)
-    if existing_spread:
-        raise Exception("Spread already exists for this fertilizer")
-
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute(
-        "INSERT INTO spread (fertilizer_id, plot_number, date, spread_quantity) VALUES (%s, %s, %s, %s)",
-        (fertilizer_id, plot_number, date, spread_quantity),
-    )
-    conn.commit()
-    conn.close()
+    try:
+        cur.execute(
+            "INSERT INTO spread (fertilizer_id, plot_number, date, spread_quantity) VALUES (%s, %s, %s, %s)",
+            (fertilizer_id, plot_number, date, spread_quantity),
+        )
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+        conn.close()
 
 
 def update_spread(fertilizer_id, plot_number, new_date, new_spread_quantity):
@@ -110,19 +115,20 @@ def update_spread(fertilizer_id, plot_number, new_date, new_spread_quantity):
     :param (int) plot_number: The plot number.
     :param (date) new_date: The new date of the spread.
     :param (int) new_spread_quantity: The new quantity of the spread.
+    :return: Expection if not spread not exist.
     """
-    existing_spread = get_spread_by_fertilizer(fertilizer_id)
-    if not existing_spread:
-        raise Exception("Spread not found for this fertilizer")
-
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute(
-        "UPDATE spread SET spread_quantity = %s WHERE fertilizer_id = %s AND plot_number = %s AND date = %s",
-        (new_spread_quantity, fertilizer_id, plot_number, new_date),
-    )
-    conn.commit()
-    conn.close()
+    try:
+        cur.execute(
+            "UPDATE spread SET spread_quantity = %s WHERE fertilizer_id = %s AND plot_number = %s AND date = %s",
+            (new_spread_quantity, fertilizer_id, plot_number, new_date),
+        )
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+        conn.close()
 
 
 def partial_update_spread(fertilizer_id, plot_number, new_date, new_spread_quantity):
@@ -133,20 +139,21 @@ def partial_update_spread(fertilizer_id, plot_number, new_date, new_spread_quant
     :param (int) plot_number: The UUID of the plot.
     :param (date) new_date: The new date of the spread.
     :param (int) new_spread_quantity: The new spread quantity.
+    :return: Expection if not spread not exist.
     """
-    existing_spread = get_spread_by_fertilizer(fertilizer_id)
-    if not existing_spread:
-        raise Exception("Spread not found for this fertilizer")
-
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute(
-        "UPDATE public.spread SET date = COALESCE(%s, date), spread_quantity = COALESCE(%s, spread_quantity) WHERE "
-        "fertilizer_id = %s AND plot_number = %s",
-        (new_date, new_spread_quantity, fertilizer_id, plot_number),
-    )
-    conn.commit()
-    conn.close()
+    try:
+        cur.execute(
+            "UPDATE public.spread SET date = COALESCE(%s, date), spread_quantity = COALESCE(%s, spread_quantity) WHERE "
+            "fertilizer_id = %s AND plot_number = %s",
+            (new_date, new_spread_quantity, fertilizer_id, plot_number),
+        )
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+        conn.close()
 
 
 def delete_spread(fertilizer_id):
@@ -154,12 +161,15 @@ def delete_spread(fertilizer_id):
     Delete spread by fertilizer_id.
 
     :param (str) fertilizer_id: The UUID of the fertilizer.
+    :return: Expection if not spread not exist.
     """
-    existing_spread = get_spread_by_fertilizer(fertilizer_id)
-    if not existing_spread:
-        raise Exception("Spread not found for this fertilizer")
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("DELETE FROM public.spread WHERE fertilizer_id = %s", (fertilizer_id,))
-    conn.commit()
-    conn.close()
+    try:
+        cur.execute("DELETE FROM public.spread WHERE fertilizer_id = %s", (fertilizer_id,))
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+        conn.close()
+
