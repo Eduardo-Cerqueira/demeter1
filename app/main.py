b44ba8d80c500cc446cd49c117a6a40d23326fdf
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-11-03 00:03:56 +0100

    :sparkles: Add cultures router to main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routers import units, productions
+from app.routers import units, productions, cultures
 
 app = FastAPI(title="Demeter", version="0.1.0")
 
 
 app.include_router(units.router)
 app.include_router(productions.router)
+app.include_router(cultures.router)
 
 
 @app.get("/")
