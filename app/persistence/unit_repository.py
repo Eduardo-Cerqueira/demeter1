b72b2c716fde49a28f4b8496eff359c91eb2b41b
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-11-02 13:40:05 +0100

    :card_file_box: Fix unit update

--- app/persistence/unit_repository.py
+++ app/persistence/unit_repository.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import psycopg2
 import os
 from dotenv import load_dotenv
@@ -21,11 +23,11 @@ conn = psycopg2.connect(
 db = conn.cursor()
 
 
-def fetch_all_unit() -> list[str] | Exception:
+def fetch_all_unit() -> list[tuple[Any, ...]] | Exception:
     """Returns a list of string representing all units inside the table unit.
     If this nothing is found, it will return a single empty array.
     :return: A list of string unit
-    :rtype: list[str] | Exception
+    :rtype: list[tuple[str]] | Exception
     """
     try:
         db.execute("SELECT * FROM unit")
@@ -34,11 +36,11 @@ def fetch_all_unit() -> list[str] | Exception:
         return error
 
 
-def fetch_unit_by_unit(unit: str) -> list[str] | Exception:
+def fetch_unit_by_unit(unit: str) -> tuple[Any, ...] | None | Exception:
     """Returns a list of string representing a unit filtered by the unit field inside the table unit.
     If this nothing is found, it will return a single empty array.
     :return: A list of string unit
-    :rtype: list[str] | Exception
+    :rtype: list[tuple[str]] | Exception
     """
     try:
         db.execute("SELECT * FROM unit WHERE unit = %s", [unit])
@@ -71,6 +73,24 @@ def update_unit(unit_value: str, unit: str) -> None | Exception:
     """
     try:
         db.execute("UPDATE unit SET unit = %s WHERE unit = %s", [unit_value, unit])
+    except Exception as error:
+        return error
+    finally:
+        conn.commit()
+
+
+def partial_update_unit(unit_value: str, unit: str) -> None | Exception:
+    """Partially update unit row using his unit field to filter from table unit.
+    :parameter unit_value:
+    :parameter unit:
+    :return: Nothing or an error
+    :rtype: None | Exception
+    """
+    try:
+        db.execute(
+            "UPDATE unit SET unit = COALESCE(%s, unit) WHERE unit = %s",
+            [unit_value, unit],
+        )
     except Exception as error:
         return error
     finally:
--- app/routers/units.py
+++ app/routers/units.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
 
@@ -7,6 +9,7 @@ from app.persistence.unit_repository import (
     insert_unit,
     update_unit,
     delete_unit,
+    partial_update_unit,
 )
 
 router = APIRouter(
@@ -17,6 +20,10 @@ router = APIRouter(
 
 class Unit(BaseModel):
     unit: str
+
+
+class UnitOptional(BaseModel):
+    unit: Optional[str] = None
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -63,6 +70,19 @@ def update_unit_by_unit(unit_name: str, unit: Unit):
             headers={"X-Error": "Resource not found"},
         )
     return update_unit(unit_value=unit.unit, unit=unit_name)
+
+
+@router.patch("/{unit_name}", status_code=status.HTTP_204_NO_CONTENT)
+def update_partial_unit_by_unit(unit_name: str, unit: UnitOptional):
+    db_unit = fetch_unit_by_unit(unit=unit_name)
+    stored_model = UnitOptional(**unit.model_dump())
+    if not db_unit:
+        raise HTTPException(
+            status_code=404,
+            detail="Unit not found",
+            headers={"X-Error": "Resource not found"},
+        )
+    return partial_update_unit(unit_value=stored_model.unit, unit=unit_name)
 
 
 @router.delete("/{unit}", status_code=status.HTTP_204_NO_CONTENT)
