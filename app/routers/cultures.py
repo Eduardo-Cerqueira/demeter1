b117e03e13b48c5121d8591164bd348090a68c92
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-11-02 23:58:52 +0100

    :construction: Add route to create a culture

--- app/routers/cultures.py
+++ app/routers/cultures.py
@@ -64,3 +64,36 @@ def read_cultures():
     - **quantity**: each quantity have integer value
     """
     return {"data": fetch_all_culture()}
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, summary="Create one culture")
+def create_culture(culture: Culture):
+    """
+    Create a culture with all the information:
+
+    - **plot_number**: plot_number associated with the culture
+    - **production_code**: production_code associated with the culture
+    - **start_date**: each start_date have a datetime
+    - **end_date**: each end_date have a datetime
+    - **quantity**: each quantity have integer value
+    """
+    if not culture.id:
+        raise HTTPException(
+            status_code=400,
+            detail="Culture is empty",
+            headers={"X-Error": "Resource empty"},
+        )
+    db_culture = fetch_culture_by_id(culture_id=culture.id)
+    if db_culture is not None:
+        raise HTTPException(
+            status_code=409,
+            detail="Culture already exists",
+            headers={"X-Error": "Resource already exists"},
+        )
+    return insert_culture(
+        plot_number=culture.plot_number,
+        production_code=culture.production_code,
+        start_date=culture.start_date,
+        end_date=culture.end_date,
+        quantity=culture.quantity,
+    )
