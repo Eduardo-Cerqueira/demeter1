5ec848a1fd84636f68472365b0216e6a766c6efb
JeffY1710 <jeff.yabas@gmail.com>
2023-11-03 13:35:33 +0100

    add variable type UUID

--- app/routers/fertilizers.py
+++ app/routers/fertilizers.py
@@ -1,9 +1,12 @@
 from typing import Optional
+from uuid import UUID
+
 from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
 from app.persistence.fertilizer_repository import (
     get_fertilizers,
     get_fertilizer_by_id,
+    get_fertilizer_by_name,
     create_fertilizer,
     update_fertilizer,
     partial_update_fertilizer,
@@ -41,7 +44,7 @@ def read_fertilizers(skip: int = 0, limit: int = 10):
 
 
 @router.get("/{identifier}", status_code=status.HTTP_200_OK, summary="Get fertilizer by his id")
-def read_fertilizer_by_id(identifier):
+def read_fertilizer_by_id(identifier: UUID):
     """
     Get a fertilizer resource by its identifier.
 
@@ -50,10 +53,7 @@ def read_fertilizer_by_id(identifier):
     """
     fertilizer = get_fertilizer_by_id(identifier)
     if not fertilizer:
-        raise HTTPException(
-            status_code=404,
-            detail="Fertilizer not found",
-        )
+        raise HTTPException(status_code=404, detail=f"Fertilizer {identifier} not found")
     return {"status": status.HTTP_200_OK, "data": fertilizer}
 
 
@@ -62,17 +62,23 @@ def create_fertilizers(fertilizer: Fertilizer):
     """
     Create a new fertilizer.
 
+    :param name: The new fertilizer's name
     :param fertilizer: The new fertilizer's data.
     :param fertilizer.unit: The fertilizer unit.
     :param fertilizer.name: The fertilizer's name.
     :return dict: A dict of the query response.
     """
+    existing_fertilizer = get_fertilizer_by_name(fertilizer.name)
+    if existing_fertilizer is not None:
+        raise HTTPException(
+            status_code=409, detail=f"Fertilizer {fertilizer.name} already exists"
+        )
     create_fertilizer(fertilizer.unit, fertilizer.name)
     return {"status": status.HTTP_201_CREATED, "message": "Fertilizer created"}
 
 
 @router.put("/{identifier}", status_code=status.HTTP_204_NO_CONTENT, summary="Update a fertilizer by his id")
-def update_fertilizer_by_id(identifier, fertilizer: Fertilizer):
+def update_fertilizer_by_id(identifier: UUID, fertilizer: Fertilizer):
     """
     Update a fertilizer by his identifier.
 
@@ -84,15 +90,15 @@ def update_fertilizer_by_id(identifier, fertilizer: Fertilizer):
     if not fertilizer_db:
         raise HTTPException(
             status_code=404,
-            detail="Unit not found",
+            detail="Fertilizer not found",
             headers={"X-Error": "Resource not found"},
         )
-    update_fertilizer(identifier, fertilizer.unit, fertilizer.name)
+    update_fertilizer(str(identifier), fertilizer.unit, fertilizer.name)
     return {"message": "Fertilizer updated"}
 
 
 @router.delete("/{identifier}", status_code=status.HTTP_204_NO_CONTENT, summary="Delete a fertilizer by his id")
-def delete_fertilizer_by_id(identifier):
+def delete_fertilizer_by_id(identifier: UUID):
     """
     Delete a fertilizer by his identifier.
 
@@ -100,14 +106,14 @@ def delete_fertilizer_by_id(identifier):
     :return dict: A dict of the query response.
     """
     fertilizer_to_delete = get_fertilizer_by_id(identifier)
-    if not fertilizer_to_delete:
+    if fertilizer_to_delete is None:
         raise HTTPException(status_code=404, detail=f"Fertilizer {identifier} not found")
-    delete_fertilizer(identifier)
+    delete_fertilizer(str(identifier))
     return {"message": f"Fertilizer {identifier} successfully deleted"}
 
 
 @router.patch("/{identifier}", status_code=status.HTTP_204_NO_CONTENT, summary="Update partially a fertilizer")
-def partial_update_fertilizer_by_id(identifier, fertilizer: FertilizerPatch):
+def partial_update_fertilizer_by_id(identifier: UUID, fertilizer: FertilizerPatch):
     """
     Partial update a fertilizer by its identifier.
 
@@ -118,5 +124,5 @@ def partial_update_fertilizer_by_id(identifier, fertilizer: FertilizerPatch):
     fertilizer_to_update = get_fertilizer_by_id(identifier)
     if not fertilizer_to_update:
         raise HTTPException(status_code=404, detail=f"Fertilizer {identifier} not found")
-    partial_update_fertilizer(identifier, fertilizer.unit, fertilizer.name)
+    partial_update_fertilizer(str(identifier), fertilizer.unit, fertilizer.name)
     return {"message": f"Fertilizer {identifier} successfully updated"}
